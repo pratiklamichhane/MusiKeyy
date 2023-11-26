f0bf7dce2e3d8b17dc385d3300505c5aec74b603
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2023-11-26 16:11:54 +0545

    added forms for basic operation

--- musikeyy/base_app/urls.py
+++ musikeyy/base_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.index , name="index"),
+    path('', views.index, name="index"),
     path('<int:id>/', views.view_song, name="viewsong"),
+    path('add-artist/', views.create_artist, name="add_artist"),
+    path('add-song/', views.create_song, name="add_song"),
+    path('add-album/', views.create_album, name="add_album"),
 ]
+
--- musikeyy/base_app/views.py
+++ musikeyy/base_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render , get_object_or_404, redirect
 from django.urls import reverse
 from .models import Song , Artist , Album
-
+from .forms import ArtistForm, AlbumForm, SongForm
 
 # Create your views here.
 def index(request):
@@ -23,3 +23,34 @@ def index(request):
 def view_song(request, id):
     song = Song.objects.get(pk=id)
     return HttpResponseRedirect(reverse('index'))
+
+def create_artist(request):
+    if request.method == 'POST':
+        form = ArtistForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to a success page or another view
+            return redirect('/')
+    else:
+        form = ArtistForm()
+    return render(request, 'base_app/add_artist.html', {'form': form})
+
+def create_album(request):
+    if request.method == 'POST':
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = AlbumForm()
+    return render(request, 'base_app/add_album.html', {'form': form})
+
+def create_song(request):
+    if request.method == 'POST':
+        form = SongForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/') 
+    else:
+        form = SongForm()
+    return render(request, 'base_app/add_song.html', {'form': form})
