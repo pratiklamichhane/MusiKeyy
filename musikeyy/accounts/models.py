f200bf9270261dccdeadd942b08c545a9df6bec7
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2023-11-28 17:54:47 +0545

    added profile and bio

--- musikeyy/accounts/models.py
+++ musikeyy/accounts/models.py
@@ -1,2 +1,7 @@
 from django.db import models
-# Create your models here.
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
