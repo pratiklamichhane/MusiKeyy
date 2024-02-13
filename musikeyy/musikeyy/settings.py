3cf21487e7f953bef447e4e5f9a070efe8344339
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2024-02-13 23:51:53 +0545

    Update database settings in settings.py

--- musikeyy/musikeyy/settings.py
+++ musikeyy/musikeyy/settings.py
@@ -72,24 +72,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'musikeyy.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
-#         'NAME': 'musikeyy',
-#         'USER' : 'postgres',
-#         'PASSWORD' : 'admin',
-#         'HOST' : 'localhost',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'NAME': 'musikeyy',
+        'USER' : 'postgres',
+        'PASSWORD' : 'admin',
+        'HOST' : 'localhost',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
